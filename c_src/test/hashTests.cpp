3c72263c0bdf02a47a0d7fb62aaf79ffea258a4f
pippy360 <tomnomnom1@gmail.com>
2017-01-26 15:42:48 +0000

    working test case, no exception thrown anymore

--- c_src/test/hashTests.cpp
+++ c_src/test/hashTests.cpp
@@ -13,15 +13,19 @@ TEST(AverageHash, basicSerialiseTest){
 }
 
 TEST(AverageHash, basicImageTest){
-    string img1FullPath = "./resources/dist_1_7ca4a6eee2a32515-7ca4a6ee62a32515_1.jpg";
+    string img1FullPath = "./c_src/test/resources/dist_1_7ca4a6eee2a32515-7ca4a6ee62a32515_1.jpg";
     auto img1 = cv::imread(img1FullPath);
+    // cv::imshow(img1FullPath, img1);
 	ShapeAndPositionInvariantImage s_img1("img1", img1, vector<Keypoint>(), img1FullPath);
-    string img2FullPath = "./resources/dist_1_7ca4a6eee2a32515-7ca4a6ee62a32515_2.jpg";
+    string img2FullPath = "./c_src/test/resources/dist_1_7ca4a6eee2a32515-7ca4a6ee62a32515_2.jpg";
     auto img2 = cv::imread(img2FullPath);
+    // cv::imshow(img2FullPath, img2);
 	ShapeAndPositionInvariantImage s_img2("img2", img2, vector<Keypoint>(), img2FullPath);
 
     // AverageHash testHash("7ca4a6eee2a32515");
-    AverageHash testHash(s_img1);
-    EXPECT_EQ("7ca4a6eee2a32515", testHash.toString());
+    AverageHash testHash1(s_img1);
+    AverageHash testHash2(s_img2);    
+    EXPECT_EQ("7ca4a6eee2a32515", testHash1.toString());
+    EXPECT_EQ("7ca4a6eee2a32515", testHash2.toString());
 }
 
