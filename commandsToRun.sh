9b2d248209b3f4bb18091274eac1f4d71a2b32ad
Tom Murphy <pippy360@users.noreply.github.com>
2017-06-28 15:38:35 +0100

    Update commandsToRun.sh

--- commandsToRun.sh
+++ commandsToRun.sh
@@ -1,14 +1,12 @@
-sudo apt-get install git
+sudo apt-get install git cmake g++ redis-server libboost-all-dev -y
+sudo apt-get install opencv #replace with actual command
+
 git clone git://github.com/pippy360/imageHashTesting
-sudo apt-get install cmake
 rm ./CMakeCache.txt
-sudo apt-get install g++
-sudo apt-get install redis-server
-sudo apt-get install libboost-all-dev
+#cmake now works
+
 #remove annoying opencv error
 sudo ln /dev/null /dev/raw1394
-
-#cmake now works
 
 #now lets do the python server
 cd ~
